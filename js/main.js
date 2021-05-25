aff5d355c78073ab451b0fae296a1aff07e14e98
YLVN <duongy96@gmail.com>
2021-05-25 22:08:08 +0700

    footer and footnotes

--- js/main.js
+++ js/main.js
@@ -2,18 +2,21 @@
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
     return (
-        (rect.top > 0 && rect.top < (window.innerHeight - 100)) && rect.bottom > 0
+        (rect.top > 0 && rect.top < (window.innerHeight)) && rect.bottom > 0
     );
 }
 
 let listAnimate = document.querySelectorAll(".show-only-screen");
 listAnimate.forEach(function (item) {
+    item.classList.add("animate__animated");
     window.addEventListener("scroll", function () {
         if (isInViewport(item)) {
+            item.classList.add("animate__slideInUp");
             item.style.opacity = 1;
-
         } else {
+            item.classList.remove("animate__slideInUp")
             item.style.opacity = 0;
+
         }
     })
 
